806fd28dd3c708266470f7f952e92b2d4e848a71
x952897642 <952897642@qq.com>
2019-08-06 20:48:18 +0800

    增加X-Admin前端UI框架，打算替换原始UI

--- src/ABPCoreProject.Web.Mvc/Controllers/HomeController.cs
+++ src/ABPCoreProject.Web.Mvc/Controllers/HomeController.cs
@@ -1,15 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Abp.AspNetCore.Mvc.Authorization;
 using ABPCoreProject.Controllers;
+using ABPCoreProject.Web.Views.Shared.Components.SideBarNav;
+using Abp.Application.Navigation;
+using Abp.Threading;
+using Abp.Runtime.Session;
 
 namespace ABPCoreProject.Web.Controllers
 {
     [AbpMvcAuthorize]
     public class HomeController : ABPCoreProjectControllerBase
     {
+        private readonly IUserNavigationManager _userNavigationManager;
+
+        public HomeController(
+            IUserNavigationManager userNavigationManager)
+        {
+            _userNavigationManager = userNavigationManager;
+        }
         public ActionResult Index()
         {
             return View();
         }
-	}
+        public PartialViewResult IndexAdmin()
+        {
+            var model = new SideBarNavViewModel
+            {
+                MainMenu = AsyncHelper.RunSync(() => _userNavigationManager.GetMenuAsync("MainMenu", AbpSession.ToUserIdentifier()))
+            };
+            return PartialView("IndexAdmin", model);
+        }
+    }
 }
